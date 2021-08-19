a9f6caa4ab4abad4340f0d4d895424932d5345ca
Rabz92 <rabbiailyas92@gmail.com>
2021-08-19 15:23:15 +0200

    Converted react project to typescript

---
+++ src/types/Reservation.ts
@@ -0,0 +1,12 @@
+export interface Reservation {
+    _id:             string;
+    name:            string;
+    phone:           string;
+    smoking:         boolean;
+    dateTime:        string;
+    specialRequests: string;
+    numberOfPersons: number;
+    createdAt:       string;
+    updatedAt:       string;
+    __v:             number;
+}
